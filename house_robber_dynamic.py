1a1bf7f0038d336d3f8dfdd6b569c57d6ff83376
Karina Pichardo <kpichardo13@berkeley.edu>
2020-11-03 19:42:45 -0800

    "add files"

--- house_robber_dynamic.py
+++ house_robber_dynamic.py
@@ -1,7 +1,10 @@
 """
 https://leetcode.com/problems/house-robber-ii
 
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. 
+Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent 
+houses were broken into on the same night.
 Given a list of non-negative integers nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 
 Example 1:
@@ -20,4 +23,18 @@ Input: nums = [0]
 Output: 0
 """
 
-def rob(nums): return
+def rob(nums): 
+    if len(nums) < 2:
+        return nums
+    elif len(nums) == 2:
+        return max(nums)
+    else:
+        return max(helper(nums[1:]), nums[0] + helper(nums[2:-1]))
+
+def helper(nums):
+    arr = [0] * len(nums)
+
+    for i in range(2, len(nums)):
+        arr[i] = max(arr[i -1], arr[i] + arr[i-2])
+    return arr[-1]
+
